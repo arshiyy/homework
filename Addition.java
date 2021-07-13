d380ee78cd658ac64cc649ba9062c28fc43a52e5
arshiyy <87352022+arshiyy@users.noreply.github.com>
2021-07-13 09:41:57 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -15,34 +15,6 @@ public class Addition {
 		
 		
 	}
-//1. Java Program to Add Two Numbers.
-
-package Training;
-
-public class Addition {
-	public static void main(String[] args) {
-		System.out.println("enter two number");
-		
-		int num= 10;
-		int num2= 20;
-		System.out.println("Tha first num:"+num);
-		System.out.println("Tha Second num"+num2);
-		int Addition=num+num2;
-		System.out.println("Addition is " +Addition);
-		
-		
-	}
 
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
 
